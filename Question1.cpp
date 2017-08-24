27510168b62e7980a6d51f32e1f68daa46fc50ca
premnigamkar <31319234+premnigamkar@users.noreply.github.com>
2017-08-24 22:42:59 +0530

    Add files via upload

---
+++ Question1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+            int a;
+            char b;
+            float c;
+            long d;
+            double e;
+            bool f;
+            cout<<" Enter the variables of int,char,float,long,double and bool type\n";
+            cin>>a;
+            cout<<"\n";
+            cin>>b;
+            cout<<"\n";
+            cin>>c;
+            cout<<"\n";
+            cin>>d;
+            cout<<"\n";
+            cin>>e;
+            cout<<"\n";
+            cin>>f;
+            cout<<"\n";
+            cout<<" The integer is "<<a<<", the charachter is "<<b<<",the float variable is "<<c<<", the long variable is "<<d<<", the double variable is "<<e<<" and the boolean variable is "<<f;
+            
+    
+	return 0;
+}
